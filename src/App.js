7f3b0b59e2ad3b1f443c530e1ab608b945449b4d
EstebanSB <es.salinasb@duocuc.cl>
2023-05-27 00:34:21 -0400

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import imgRickMorty from './img/rick-morty.png';
 import './App.css';
 
 function App() {
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+
+    console.log(characterApi);
+    
+  
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+        <img src={imgRickMorty} alt='Rick Y Morty Img' className="img-home" /> 
+        <button onClick={reqApi} className='btn-search'>Buscar Personaje</button>
+      
       </header>
     </div>
   );
