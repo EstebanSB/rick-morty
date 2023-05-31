14c7f6d90ba5efd6aa00c1f7fb447a624ea28cae
EstebanSB <e.salinasb1@gmail.com>
2023-05-31 01:43:00 -0400

    botones para cambiar de pagina

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import imgRickMorty from './img/rick-morty.png';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Characters from './components/Characters';
 
 function App() {
   const [characters, setCharacters] = useState(null);
+  const [page, setPage] = useState(1);
 
   const reqApi = async () => {
-    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const api = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
     const characterApi = await api.json();
 
     setCharacters(characterApi.results);
+  };
+
+  useEffect(() => {
+    reqApi();
+  }, [page]);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  const previousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
   };
 
   return (
@@ -18,13 +33,18 @@ function App() {
       <header className="App-header">
         <h1 className="title">Rick Y Morty</h1>
         {characters ? (
-          <Characters characters={characters} setCharacters={setCharacters} />
+          <Characters
+            characters={characters}
+            setCharacters={setCharacters}
+            nextPage={nextPage}
+            previousPage={previousPage}
+          />
         ) : (
           <>
             <img src={imgRickMorty} alt='Rick Y Morty Img' className="img-home" /> 
             <button onClick={reqApi} className='btn-search'>Buscar Personaje</button>
           </>
-        )}      
+        )}
       </header>
     </div>
   );
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,10 +1,12 @@
+import React from 'react';
+
 export default function Characters(props) {
-  const { characters, setCharacters } = props;
+  const { characters, setCharacters, nextPage, previousPage } = props;
 
   const resetCharacters = () => {
     setCharacters(null);
-  }
-  
+  };
+
   return (
     <div className="characters">
       <h1 className='title'>Personajes</h1>
@@ -12,7 +14,7 @@ export default function Characters(props) {
       <button onClick={resetCharacters} className='btn-search'>Volver al Inicio</button>
       <div className="container-characters">
         {characters.map((character, index) => (
-          <div className="character-container" key={ index } >
+          <div className="character-container" key={index}>
             <div>
               <img src={character.image} alt={character.name} />
             </div>
@@ -21,11 +23,11 @@ export default function Characters(props) {
               <h6>
                 {character.status === "Alive" ? (
                   <>
-                  <span className="alive"/>Alive
+                    <span className="alive"/>Alive
                   </>
-                ): (
+                ) : (
                   <>
-                  <span className="dead"/>Dead
+                    <span className="dead"/>Dead
                   </>
                 )}
               </h6>
@@ -37,11 +39,20 @@ export default function Characters(props) {
                 <span className="text-grey">Especie: </span>
                 <span>{character.species}</span>
               </p>
+              <p>
+                <span className="text-grey">Locacion: </span>
+                <span>{character.origin.name}</span>
+              </p>
             </div>
           </div>
         ))}
       </div>
+      <div className="pagination-buttons" >
+        <button onClick={previousPage} disabled={characters.length === 0} className='btn-search' style={{ marginRight: '10px' }}>Anterior</button>
+        <button onClick={nextPage} className='btn-search'>Siguiente</button>
+      </div>
+      <br/>
       <button onClick={resetCharacters} className='btn-search'>Volver al Inicio</button>
     </div>
-  )
+  );
 }
